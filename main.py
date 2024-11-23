6ec5d31bc33f70a0389fab79f9d6f5f48ae3e4f8
kre8mymedia <kre8mymedia@gmail.com>
2024-11-23 10:33:57 -0600

    dotenv for dev conveniance

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 
 import traceback
 
+from dotenv import load_dotenv
 from langgraph.checkpoint.memory import MemorySaver
 
 from src.flows.chat_agent import builder
 from src.utils.stream import stream_graph_values
 from src.utils.system import read_system_message, SystemPaths
 from src.utils.visualize import visualize_graph
+load_dotenv()
 
+# Initialize graph and visualize once at start
+checkpointer = MemorySaver()
+graph = builder.compile(checkpointer=checkpointer)
+visualize_graph(graph, "chat_agent")
 
 # Chat loop
 while True:
@@ -17,10 +23,6 @@ while True:
             print("Goodbye!")
             break
 
-        # https://pypi.org/project/langgraph-checkpoint-postgres/
-        checkpointer = MemorySaver()
-        graph = builder.compile(checkpointer=checkpointer)
-        visualize_graph(graph, "terminal_interact")
         stream_graph_values(
             graph,
             {
