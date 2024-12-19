edf4e8b72a103a7118c2b2999312175791c2c60c
ryaneggz <kre8mymedia@gmail.com>
2024-12-18 22:33:30 -0600

    Wrong folder

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app.include_router(retrieve)
 app.include_router(source)
 
 public_dir = Path("src/public")
-docs_dir = Path("src/site")
+docs_dir = Path("site")
 
 # If portal is enabled and public directory exists, serve the React app at "/"
 if APP_PORTAL_ENABLED and public_dir.exists():
