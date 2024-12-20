144ac75a991854ccc1f2c6d702a6df83dd1f114a
ryaneggz <kre8mymedia@gmail.com>
2024-12-19 23:31:32 -0600

    Can build frontend to image

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,15 @@
 
 
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/login');
+    }, [navigate]);
+
     return (
         <main
             className="flex min-h-screen flex-col"
