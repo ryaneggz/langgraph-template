abe418e6d3c27892beb27575e5e6afe2ebe6b107
ryaneggz <kre8mymedia@gmail.com>
2024-12-26 21:37:13 -0600

    Takes an APP_USER_LIST

--- backend/src/constants/__init__.py
+++ backend/src/constants/__init__.py
@@ -1,23 +1,27 @@
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
+# Server
 HOST = str(os.getenv("HOST", "0.0.0.0"))
 PORT = int(os.getenv("PORT", 8000))
 LOG_LEVEL = os.getenv("LOG_LEVEL", "info")
 
+# App
+DEFAULT_VECTOR_STORE_PATH = './sandbox/db/vectorstore.json'
+APP_USER_LIST = json.loads(os.getenv("APP_USER_LIST", '[{"username": "admin", "password": "test1234", "name": "Admin User", "email": "admin@example.com"}]'))
+APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
+APP_LOG_LEVEL = os.getenv("APP_LOG_LEVEL", "INFO").upper()
+
+# Database
 DB_URI = os.getenv("POSTGRES_CONNECTION_STRING", "postgresql://admin:test1234@localhost:5432/lg_template_dev?sslmode=disable")
 DB_URI_SANDBOX = os.getenv("POSTGRES_CONNECTION_STRING_SANDBOX", "postgresql://admin:test1234@localhost:5432/lg_template_agent?sslmode=disable")
 CONNECTION_POOL_KWARGS = {
     "autocommit": True,
     "prepare_threshold": 0,
 }
-DEFAULT_VECTOR_STORE_PATH = './sandbox/db/vectorstore.json'
-APP_USERNAME = os.getenv("APP_USERNAME", "admin")
-APP_PASSWORD = os.getenv("APP_PASSWORD", "test1234")
-APP_VERSION = os.getenv("APP_VERSION", "0.1.0")
-APP_LOG_LEVEL = os.getenv("APP_LOG_LEVEL", "INFO").upper()
-APP_PORTAL_ENABLED = os.getenv("APP_PORTAL_ENABLED")
 
+# LLM API Keys
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
