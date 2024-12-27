71be787b5f66c6d5dafd5e68754efd08f2b95e49
Ryan Eggleston <40816745+ryaneggz@users.noreply.github.com>
2024-12-26 22:44:50 -0600

    FROM feature/77-basic-auth-json-defined-users TO development (#78)
    * init
    
    * Takes an APP_USER_LIST

--- backend/src/utils/auth.py
+++ backend/src/utils/auth.py
@@ -1,25 +1,29 @@
 from fastapi import status
-from fastapi.security import  HTTPBasicCredentials, HTTPBasic
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
 from fastapi import Depends, HTTPException
-from src.constants import APP_PASSWORD, APP_USERNAME
 import secrets
+from src.constants import APP_USER_LIST
 
 def verify_credentials(credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
-    current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = APP_USERNAME.encode("utf8")
-    is_correct_username = secrets.compare_digest(
-        current_username_bytes, correct_username_bytes
-    )
-    current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = APP_PASSWORD.encode("utf8")
-    is_correct_password = secrets.compare_digest(
-        current_password_bytes, correct_password_bytes
-    )
-    
-    if not (is_correct_username and is_correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
+    for user in APP_USER_LIST:
+        current_username_bytes = credentials.username.encode("utf8")
+        correct_username_bytes = user["username"].encode("utf8")
+        is_correct_username = secrets.compare_digest(
+            current_username_bytes, correct_username_bytes
         )
-    return credentials.username
+        
+        if is_correct_username:
+            current_password_bytes = credentials.password.encode("utf8")
+            correct_password_bytes = user["password"].encode("utf8")
+            is_correct_password = secrets.compare_digest(
+                current_password_bytes, correct_password_bytes
+            )
+            
+            if is_correct_password:
+                return user
+                
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Basic"},
+    ) 
